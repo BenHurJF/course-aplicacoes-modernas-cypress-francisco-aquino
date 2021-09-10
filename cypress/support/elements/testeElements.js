e4ab85822bce3ae3a82e9d8f59934f8214543ed7
Ben-Hur Jeffer <67138155+BenHurJF@users.noreply.github.com>
2021-09-10 02:43:15 -0300

    tst

--- cypress/support/elements/testeElements.js
+++ cypress/support/elements/testeElements.js
@@ -1,4 +1,3 @@
-
-
-
-export default testeElements;
+export const ELEMENTS = {
+    teste: '.name'
+}
--- cypress/support/pageObjects/teste.js
+++ cypress/support/pageObjects/teste.js
@@ -1,2 +1,11 @@
 /// <reference types="cypress" />
 
+const element = require('../elements/testeElements').ELEMENTS;
+
+class teste {
+     fazerLogin() {
+         cy.get(element.teste).should('be.visible');
+     }
+}
+
+export default new teste;
