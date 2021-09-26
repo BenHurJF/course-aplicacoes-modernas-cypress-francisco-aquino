606e9311a2f8c0e2349f88e51c54cfc2a5129a44
Ben-Hur Jeffer <67138155+BenHurJF@users.noreply.github.com>
2021-09-25 23:49:56 -0300

    uso-timeout

--- cypress/integration/Sincronismo Cypress.spec.js
+++ cypress/integration/Sincronismo Cypress.spec.js
@@ -1,40 +1,61 @@
 /// <reference types= "Cypress" />
 
 describe('Sincronismo Cypress', () => {
-    
+
     beforeEach(() => {
         cy.fixture('teste').then((teste) => {
-            cy.visit(teste.url);    
-    }); 
-});
-    
+            cy.visit(teste.url);
+        });
+    });
+
     it('Deve aguardar elemento ficar disponível', () => {
         cy.get('#novoCampo')
-        .should('not.exist')
+            .should('not.exist')
 
         cy.get('#buttonDelay')
-        .click()
+            .click()
         // ainda nao deve aparecer, pois tem o delay
         cy.get('#novoCampo')
-        .should('not.exist')
+            .should('not.exist')
 
         // ate que um determinado momento vai existir
         cy.get('#novoCampo')
-        .should('exist')
+            .should('exist')
 
         cy.get('#novoCampo')
-        .type('Testes com Cypress')
-        .should('be.visible')
+            .type('Testes com Cypress')
+            .should('be.visible')
     });
 
     it('Deve fazer Retrys', () => {
         cy.get('#novoCampo')
-        .should('not.exist')
+            .should('not.exist')
 
+        cy.get('#buttonDelay')
+            .click()
+
+        cy.get('#novoCampo')
+            .should('exist')
+    })
+
+    it('Uso do Find', () => {
+        cy.get('#buttonList').click();
+
+        cy.get('#lista li')
+            .find('span')
+            .should('contain', 'Item 1');
+
+        ///
+        /// cy.wait(3000) UTILIZAR O TIMEOUT PARA ESPERA ESPECÍFICA EM ELEMENTOS
+        ///
+        cy.get('#lista li span')
+            .should('contain', 'Item 2')
+    })
+
+    it.only('Uso do timeout', () => {
         cy.get('#buttonDelay')
         .click()
 
-        cy.get('#novoCampo')
-        .should('exist')
+        cy.get('#novoCampo', { timeout: 4000 }).should('exist')
     })
 })
