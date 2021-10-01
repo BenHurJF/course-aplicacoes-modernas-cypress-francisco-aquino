5ce9f6750b2e15bbf27ce73ae4c9973d3c42da33
Ben-Hur Jeffer <67138155+BenHurJF@users.noreply.github.com>
2021-10-01 02:43:24 -0300

    localizando-elementos

---
+++ cypress/integration/LocalizandoElementos.spec.js
@@ -0,0 +1,4 @@
+//
+// Para mduar a ordem de busca de elementos na tela vá no site do cypress e busque por -> Playground Selector
+// e veja a prioridade que deseja para o cypress buscar no Seletor de elemento e defina o comando em -> Support/index
+//
