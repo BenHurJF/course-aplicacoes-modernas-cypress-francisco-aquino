614865d73745133d7b1abaf58aaa631a2866dbdb
Ben-Hur Jeffer <67138155+BenHurJF@users.noreply.github.com>
2021-10-01 02:24:35 -0300

    Pontos de Atenção-Alerts-Confirms-Pop-ups-etc

--- cypress/integration/PontosImportantes.spec.js
+++ cypress/integration/PontosImportantes.spec.js
@@ -71,7 +71,7 @@ describe('Pontos Importantes para soluções', () => {
         cy.get('#confirm').click()
     })
 
-    it.only('Trabalhando com PROMPT do navegador', () => {
+    it('Trabalhando com PROMPT do navegador', () => {
         cy.window().then(win => { // cy.window = Será um elemento window, então '.then' execute a função, usando o parâmetro win, cy.stub pega o parâmetro window do tipo 'prompt'
             cy.stub(win, 'prompt').returns('12')
         })
@@ -84,4 +84,97 @@ describe('Pontos Importantes para soluções', () => {
 
         cy.get('#prompt').click()
     })
+
+    it('Desafio - Validar Mensagens', () => {
+      
+        // Para MUITOS Alerts - Utilizamos STUBS
+       // Botão cadastrar - Nome obrigatorio
+      const stub = cy.stub().as('alerta') // para Usar e validar os alerts que virá a parecer na tela
+      cy.on('window:alert', stub) // Atribuindo os alerts que aparecer na tela ao Stub
+
+      cy.get('#formCadastrar').click()
+      .then(() => expect(stub.getCall(0)).to.be.calledWith('Nome eh obrigatorio'))
+
+
+       // Preenchendo nome e -> cadastrar -> Sobrenome Obrigatorio
+       cy.get('#formNome').type('BenHur')
+       .get('#formCadastrar').click()
+       .then(() => expect(stub.getCall(1)).to.be.calledWith('Sobrenome eh obrigatorio'))
+
+       // Preenchendo nome e Sobrenome -> Cadastrar -> Sexo é obrigatorio
+       cy.get('#formSobrenome').type('Jeffer')
+       .get('#formCadastrar').click()
+       .then(() => expect(stub.getCall(2)).to.be.calledWith('Sexo eh obrigatorio'))
+       
+       // Verificando palavra 'Cadastrado'
+       cy.get('#formSexoMasc').click()
+       cy.get('#formCadastrar').click()
+       cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
+    })
+})
+    
+describe('iFRAMES - Testando na página inicial - e página EXTERNA', () => {
+    it('iFrames...Testando', () => {
+        cy.visit(Cypress.env('url'))
+       
+        cy.get('#frame1').then( iframe => {
+            // .contents = trás os filhos desse elemento, podendo da um .find('e aqui buscar o elemento')
+            const body = iframe.contents().find('body') // Primeiro pegando o Body, e depois dando find em um elemento específico
+            cy.wrap(body).find('#tfield')
+            .type('Funcionou ?') // dando um type no campo
+            .should('have.value', 'Funcionou ?') // e verificando se contém o texto nesse input
+            
+        /*    cy.on('window:alert', buttonIframe => { // padrão de alert, verificando se o alert que vai aparecer é igual ao texto -> Alert Simples
+              expect(buttonIframe).to.be.equal('Alert Simples')
+            })
+            cy.wrap(body).find('#otherButton') // aqui dando um find em outro elemento dentro do body, que no caso é o botão e clicando
+            .click()
+        })   */
+    })
+})
+
+    it('iFrames...Testando diretamente da página do iFrame', () => {
+        cy.visit('https://wcaquino.me/cypress/frame.html')
+
+        cy.on('window:alert', buttonIframe => { // padrão de alert, verificando se o alert que vai aparecer é igual ao texto -> Alert Simples
+            expect(buttonIframe).to.be.equal('Click OK!')
+          })
+          cy.get('#otherButton') // aqui dando um find em outro elemento dentro do body, que no caso é o botão e clicando
+          .click()
+    })
+})
+
+describe('POP-UPs diretamente', () => {
+    it('pop-ups', () => {
+        cy.visit('https://wcaquino.me/cypress/frame.html')
+
+        cy.get('#otherButton').click()
+        cy.on('window:alert', alertPopUp => {
+            expect(alertPopUp).to.be.equal('Click OK!')
+        })
+    })
+
+    it('Deve verificar se o pop-up foi invocado', () => {
+        cy.visit(Cypress.env('url'))
+        
+        cy.window().then(win => { // PEGANDO UM EVENTO DE BROWSER E CHAMANDO PRO PARÂMETRO -> win
+            cy.stub(win, 'open').as('winOpen') // Verificando que o pop-up de tela foi aberto, ele é do tipo OPEN
+        })
+        cy.get('#buttonPopUp').click()
+        cy.get('@winOpen').should( 'be.called') // Validando que esse evento foi chamado = de abrir um pop-up/tela
+})
+})
+
+describe.only('POP-UPs através de LINKS', () => {
+     before(() => {
+        cy.visit('https://wcaquino.me/cypress/componentes.html')
+     })
+
+     it('pop-up URL', () => {
+         cy.contains('Popup2')
+         .should('have.prop', 'href')
+         .and('equal', 'https://wcaquino.me/cypress/frame.html')
+     })
+
+
 })
