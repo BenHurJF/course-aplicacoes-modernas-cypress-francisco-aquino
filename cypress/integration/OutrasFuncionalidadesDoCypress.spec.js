9aa50ae6d6736de4ff3dea41124ec3b5bc2ccf93
Ben-Hur Jeffer <67138155+BenHurJF@users.noreply.github.com>
2021-10-16 20:01:19 -0300

    testes

--- cypress/integration/OutrasFuncionalidadesDoCypress.spec.js
+++ cypress/integration/OutrasFuncionalidadesDoCypress.spec.js
@@ -4,41 +4,41 @@
 
 describe('Fixtures testes', () => {
     before('', () => {
-    //    cy.visit('https://wcaquino.me/cypress/componentes.html')
+        //    cy.visit('https://wcaquino.me/cypress/componentes.html')
     })
-       
-   
+
+
 
     it('Get DATA from fixture file', function () {
         cy.fixture('userData').as('site').then(() => {
             cy.visit(this.site.url)
         })
-        
+
 
         cy.fixture('userData').as('usuario').then(() => {
             cy.get('#formNome').type(this.usuario.nome)
-            .should('have.value', this.usuario.nome)
+                .should('have.value', this.usuario.nome)
             cy.get('#formSobrenome').type(this.usuario.sobrenome)
-            .should('have.value', this.usuario.sobrenome)
-         })
+                .should('have.value', this.usuario.sobrenome)
+        })
     })
 
     it('Commands...', () => {
-       cy.popup('#otherButton', 'Click OK!')
+        cy.popup('#otherButton', 'Click OK!')
     })
 
 
-describe('Testes Dinâmicos...', () => {
-        before('', () => {
-           cy.visit('https://wcaquino.me/cypress/componentes.html')
+    describe('Testes Dinâmicos...', () => {
+        beforeEach('', () => {
+            cy.visit('https://wcaquino.me/cypress/componentes.html')
         })
 
         const food = ['Carne', 'Frango', 'Pizza', 'Vegetariano']
-    it.only('Cadastro com comidas variáveis...', () => {
-        cy.get('#formComidaCarne')
-        cy.get('#formComidaFrango')
-        cy.get('#formComidaPizza')
-        cy.get('#formComidaVegetariana')
-     })
-})
+
+        it.only('Cadastro com comidas variáveis...', () => {
+            cy.get('#formNome').type('Testando')
+            cy.get('#formSobrenome').type('Ben-Hur')
+
+        })
+    })
 })
