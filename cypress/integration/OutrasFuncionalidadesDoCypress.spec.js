0cc07341163ccb24f8a430282541066768dde8c8
Ben-Hur Jeffer <67138155+BenHurJF@users.noreply.github.com>
2021-10-17 01:31:29 -0300

    Testes Dinâmicos

--- cypress/integration/OutrasFuncionalidadesDoCypress.spec.js
+++ cypress/integration/OutrasFuncionalidadesDoCypress.spec.js
@@ -34,11 +34,47 @@ describe('Fixtures testes', () => {
         })
 
         const food = ['Carne', 'Frango', 'Pizza', 'Vegetariano']
-
-        it.only('Cadastro com comidas variáveis...', () => {
+        food.forEach( food => {
+        it(`Cadastro com comidas: ${food}...`, () => {
             cy.get('#formNome').type('Testando')
             cy.get('#formSobrenome').type('Ben-Hur')
+            cy.get(`table[id='formSexo'] input[id='formSexoMasc']`).check()
+            cy.xpath(`//label[contains(.,'${food}')]/../input`).check()
+            cy.get('#formEscolaridade').select('doutorado')
+            cy.get('#formEsportes').select('futebol')
+            cy.get(`input[value='Cadastrar']`).click()
+            cy.xpath(`//span[contains(.,'Cadastrado')]`).should('contain', 'Cadastrado')
+        })
+        })
 
+        it.only('Deve selecionar todos usando o EACH', () => {
+            cy.get('#formNome').type('Testando')
+            cy.get('#formSobrenome').type('Ben-Hur')
+            cy.get(`table[id='formSexo'] input[id='formSexoMasc']`).check()
+
+            // cy.get(`[name=formComidaFavorita]`).click({multiple:true}) // OU ->>
+
+            cy.get(`[name=formComidaFavorita]`).each($elem => { 
+                // Vai clicar em cada um dos checkbox desse formComidaFavorita sendo de acordo
+                // com a condição -> se for diferente de vegetariano = pode clicar.
+                if($elem.val() != 'vegetariano')
+                cy.wrap($elem).click()
+            })
+
+            cy.get('#formEscolaridade').select('doutorado')
+            cy.get('#formEsportes').select('futebol')
+            cy.get(`input[value='Cadastrar']`).click()
+            cy.xpath(`//span[contains(.,'Cadastrado')]`).should('contain', 'Cadastrado')
+            // cy.clickAlert('#formCadastrar', 'Tem certeza que voce eh vegetariano?')
         })
     })
+
+    describe('Testes Dinâmicos...', () => {
+        beforeEach('', () => {
+            cy.visit('https://wcaquino.me/cypress/componentes.html')
+        })
+    it('Time', () => {
+      
+    })
+})
 })
